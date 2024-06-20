eab94ced75fd2e3ee4ba8232f230d4690139a705
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-06-20 17:25:15 +0300

    grayscale for image in WhatYuoGet Section

--- src/entities/WhatYouGet/ui/WhatYouGetItem.tsx
+++ src/entities/WhatYouGet/ui/WhatYouGetItem.tsx
@@ -4,10 +4,9 @@ import { OptionList } from "./OptionItem"
 
 const WhatYouGetItem = ({ imageUrl, title, options }: WhatYouGetList) => (
   <div className="flex flex-col gap-y-5 md:flex-row md:even:flex-row-reverse">
-    <div>
+    <div className=" grayscale-[80%] ">
       <Image src={imageUrl} width={521} height={355} alt="div" />
     </div>
-
     <div className="flex flex-col justify-center">
       <h3 className="mb-[17px] text-lg font-bold text-primary-500">{title}</h3>
 
