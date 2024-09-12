186eab8338fb07d06132660d4f7587de3bb7e702
Mac <mac@MacBook-Pro-Mac.local>
2024-09-13 00:57:25 +0400

    fix seo

---
+++ app/(front)/page.tsx
@@ -0,0 +1,11 @@
+import { HomePage } from "@/views/HomePage"
+
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Профессиональное Создание Сайтов и Продвижение",
+}
+
+export default function Home() {
+  return <HomePage />
+}
--- app/(front)/price/page.tsx
+++ app/(front)/price/page.tsx
@@ -2,7 +2,7 @@ import { CasePricePage } from "@/views/CasePricePage"
 import type { Metadata } from "next"
 
 export const metadata: Metadata = {
-  title: "Цены | Barbaris",
+  title: "Цены",
   description: "Barbaris создание интернет-магазинов и web-сервисов.",
   keywords: "разработка сайтов, next js, node js, react. Создание сайтов",
 }
