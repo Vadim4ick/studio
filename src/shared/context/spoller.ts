475516699f1c41d57c28baf34c52db34b3142cc5
Mac <mac@MacBook-Pro-Mac.local>
2024-09-15 11:32:55 +0400

    fix bags

--- src/shared/context/spoller.ts
+++ src/shared/context/spoller.ts
@@ -3,13 +3,15 @@ import { createDomain } from "effector"
 const spoller = createDomain()
 
 export const openSpollerPrice = spoller.createEvent<number | null>()
+export const setSpollerPriceId = spoller.createEvent<number>()
 
 export const $spollerPrice = spoller
   .createStore<number | null>(null)
   .on(openSpollerPrice, (spollerPrice, spoilerId) => {
-    if (spollerPrice === spoilerId) {
-      return null
-    } else {
+    if (spollerPrice !== spoilerId) {
       return spoilerId
     }
+
+    return null
   })
+  .on(setSpollerPriceId, (_, id) => id)
