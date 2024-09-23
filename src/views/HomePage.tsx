4e6d426623b156c46d7e889f22fdeea26cc230e8
Mac <mac@MacBook-Pro-Mac.local>
2024-09-23 19:49:29 +0400

    add div

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -43,66 +43,64 @@ const HomePage = () => (
             height={2000}
           />
         </div>
-      </section>
+      </div>
+    </section>
 
-      <section className="mx-auto max-w-[1280px] py-10">
-        <h2 className="text mb-[25px] text-center text-lg font-extrabold md:mb-7 md:text-left  md:text-2xl lg:mb-10 lg:text-4xl">
-          Заказать Интернет Магазин:{" "}
-          <span className="text-primary-500">
-            Профессиональное Создание от Barbaris
-          </span>
-        </h2>
+    <section className="mx-auto max-w-[1280px] py-10">
+      <h2 className="text mb-[25px] text-center text-lg font-extrabold md:mb-7 md:text-left  md:text-2xl lg:mb-10 lg:text-4xl">
+        Заказать Интернет Магазин:{" "}
+        <span className="text-primary-500">
+          Профессиональное Создание от Barbaris
+        </span>
+      </h2>
 
-        <p className="text mb-[30px] text-center text-base font-extrabold md:mb-6 md:text-left md:text-xl lg:mb-8">
-          Наши ключевые услуги включают
-        </p>
-        <BenefitsList
-          benefitsData={benefits}
-          className="mb-[50px] grid grid-cols-1 justify-items-center gap-y-10 md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 lg:gap-y-[54px]"
-        />
-        <div className="mt-4 flex justify-center gap-8 font-semibold">
-          <BtnOpenConsultation />
+      <p className="text mb-[30px] text-center text-base font-extrabold md:mb-6 md:text-left md:text-xl lg:mb-8">
+        Наши ключевые услуги включают
+      </p>
+      <BenefitsList
+        benefitsData={benefits}
+        className="mb-[50px] grid grid-cols-1 justify-items-center gap-y-10 md:grid-cols-2 md:gap-x-5 lg:grid-cols-3 lg:gap-y-[54px]"
+      />
+      <div className="mt-4 flex justify-center gap-8 font-semibold">
+        <BtnOpenConsultation />
+      </div>
+    </section>
+
+    <WhatYouGet />
+
+    <section className="mx-auto max-w-[1280px] pb-10 lg:pb-24">
+      <h2 className="text mb-[60px] text-center text-lg font-extrabold md:text-2xl lg:text-4xl">
+        Сколько Стоит Создание Сайта?{" "}
+        <span className="text-primary-500">Прайс-лист Веб-Студии Barbaris</span>
+      </h2>
+
+      <div className="flex items-start gap-3">
+        <div className="text flex flex-col gap-4 tablet:basis-1/2">
+          <p>
+            Стоимость разработки сайта варьируется в зависимости от сложности
+            проекта и требуемых функциональностей. Мы предлагаем прозрачные
+            условия сотрудничества и готовы предоставить детальный расчет
+            стоимости перед началом работ.
+          </p>
+
+          <p>
+            Подробнее с ценами работ вы можете ознакомится на странице -{" "}
+            <Link className="text-primary-500" href={"/price"}>
+              цены
+            </Link>
+          </p>
         </div>
-      </section>
 
-      <WhatYouGet />
-
-      <section className="mx-auto max-w-[1280px] pb-10 lg:pb-24">
-        <h2 className="text mb-[60px] text-center text-lg font-extrabold md:text-2xl lg:text-4xl">
-          Сколько Стоит Создание Сайта?{" "}
-          <span className="text-primary-500">
-            Прайс-лист Веб-Студии Barbaris
-          </span>
-        </h2>
-
-        <div className="flex items-start gap-3">
-          <div className="text flex flex-col gap-4 tablet:basis-1/2">
-            <p>
-              Стоимость разработки сайта варьируется в зависимости от сложности
-              проекта и требуемых функциональностей. Мы предлагаем прозрачные
-              условия сотрудничества и готовы предоставить детальный расчет
-              стоимости перед началом работ.
-            </p>
-
-            <p>
-              Подробнее с ценами работ вы можете ознакомится на странице -{" "}
-              <Link className="text-primary-500" href={"/price"}>
-                цены
-              </Link>
-            </p>
-          </div>
-
-          <div className="basis-1/2 max-tablet:hidden">
-            <div className="flex items-center justify-center">
-              <CirclesOnWater />
-            </div>
+        <div className="basis-1/2 max-tablet:hidden">
+          <div className="flex items-center justify-center">
+            <CirclesOnWater />
           </div>
         </div>
-      </section>
+      </div>
+    </section>
 
-      <Stages />
-    </main>
-  )
-}
+    <Stages />
+  </main>
+)
 
 export { HomePage }
